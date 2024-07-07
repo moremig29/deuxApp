4f1eec3228fced4e9b54d8bff06ec0e63df01f97
moremig29 <miguelangel.moreira@gmail.com>
2024-07-07 13:11:56 -0400

    create table messages

--- src/app/pages/messages/messages.component.ts
+++ src/app/pages/messages/messages.component.ts
@@ -1,14 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MessageService } from '@services/message.service';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, TableModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class MessagesComponent { }
+export default class MessagesComponent {
+  constructor(public messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.messageService.getMessages();
+  }
+}
---
+++ src/app/services/message.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { computed, Injectable, signal } from '@angular/core';
+import { BaseService } from './base.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  base_url = this.baseService.base_url;
+
+  #messages = signal({
+    loading: false,
+    messages: [],
+  });
+
+  messages = computed(() => this.#messages().messages);
+  loading = computed(() => this.#messages().loading);
+
+  constructor(private http: HttpClient, private baseService: BaseService) {}
+
+  getMessages() {
+    return this.http
+      .get(`${this.base_url}/mensaje`, this.baseService.headers)
+      .subscribe((res: any) => {
+        this.#messages.set({
+          loading: false,
+          messages: res.mensajes,
+        });
+      });
+  }
+}
