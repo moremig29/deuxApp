158602f8b4408d09d086cc8ea8fe3311ce8d1cb4
moremig29 <miguelangel.moreira@gmail.com>
2024-07-30 16:21:08 -0400

    fix env

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import Swal from 'sweetalert2';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 const env = environment
 @Injectable({
